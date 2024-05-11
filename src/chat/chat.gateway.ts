1af5a6fd38c0ec2dd625f102e6c2343c7a66a725
gkesh <gkeshthapa@gmail.com>
2024-05-10 22:01:58 -0400

    Implemented socket functions

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,10 +1,44 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import {
+  SubscribeMessage,
+  WebSocketGateway,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Logger } from '@nestjs/common';
+import { Server } from 'socket.io';
 
 @WebSocketGateway()
-export class ChatGateway {
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    console.log(client, payload);
-    return 'Hello world!';
+export class ChatGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  private readonly logger = new Logger(ChatGateway.name);
+
+  @WebSocketServer() io: Server;
+
+  handleDisconnect(client: any) {
+    this.logger.debug(`Client was disconnected: ${client.id}`);
+  }
+
+  handleConnection(client: any, ...args: any[]) {
+    const { sockets } = this.io.sockets;
+
+    this.logger.log(`Client id: ${client.id} connected with args: ${args}`);
+    this.logger.debug(`Number of connected clients: ${sockets.size}`);
+  }
+
+  afterInit(server: any) {
+    this.logger.log(`Server Initialized: ${server}`);
+  }
+
+  @SubscribeMessage('ping')
+  handleMessage(client: any, payload: any) {
+    this.logger.log(`Message recieved from client: ${client.id}`);
+    this.logger.debug(`Payload: ${payload}`);
+    return {
+      event: 'pong',
+      data: 'Testing ping pong on websockets',
+    };
   }
 }
